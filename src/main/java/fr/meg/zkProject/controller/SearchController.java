a4c1a8284873505625ca16080f7beef976a067a7
nadirJavaEE <nadir.javaee@gmail.com>
2018-10-11 14:38:47 +0200

    Commit#05 - OK, need some CSS improvments

--- src/main/java/fr/meg/zkProject/controller/SearchController.java
+++ src/main/java/fr/meg/zkProject/controller/SearchController.java
@@ -1,5 +1,65 @@
 package fr.meg.zkProject.controller;
 
-public class SearchController {
+import java.util.List;
+import java.util.Set;
 
-}
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.select.SelectorComposer;
+import org.zkoss.zk.ui.select.annotation.Listen;
+import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zul.Image;
+import org.zkoss.zul.Label;
+import org.zkoss.zul.ListModelList;
+import org.zkoss.zul.Listbox;
+import org.zkoss.zul.Textbox;
+import org.zkoss.zul.ext.Selectable;
+
+import fr.meg.zkProject.business.CarService;
+import fr.meg.zkProject.business.CarServiceImpl;
+import fr.meg.zkProject.model.Car;
+
+public class SearchController extends SelectorComposer<Component> {
+
+    private static final long serialVersionUID = 1L;
+	
+    @Wire
+	private Textbox keywordBox;
+	@Wire
+	private Listbox carListbox;
+	@Wire
+	private Label modelLabel;
+	@Wire
+	private Label makeLabel;
+	@Wire
+	private Label priceLabel;
+	@Wire
+	private Label descriptionLabel;
+	@Wire
+	private Image previewImage;
+	
+	private CarService carService = new CarServiceImpl();
+	
+@Listen("onClick = #searchButton")
+	public void search(){
+		String keyword = keywordBox.getValue();
+		List<Car> result = carService.search(keyword);
+		carListbox.setModel(new ListModelList<Car>(result));
+	}
+	
+	@Listen("onSelect = #carListbox")
+	public void showDetail(){
+		Set<Car> selection = ((Selectable<Car>)carListbox.getModel()).getSelection();
+		if (selection!=null && !selection.isEmpty()){
+			Car selected = selection.iterator().next();
+			previewImage.setSrc(selected.getPreview());
+			modelLabel.setValue(selected.getModel());
+			makeLabel.setValue(selected.getMake());
+			priceLabel.setValue(selected.getPrice().toString());
+			descriptionLabel.setValue(selected.getDescription());
+		}
+	}
+}	
+	
+	
+	
+
